fb300e00aca7d40254a5c24211a4152eb94848d4
rslgp <rslgp@cin.ufpe.br>
2023-04-20 06:21:23 -0300

    creators mapa fome

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import ReactGA from 'react-ga';
 import CircularProgress from '@mui/material/CircularProgress';
 import { Checkbox } from '@mui/material';
 
+//call to action content creators
+import CreatorsMapaFome from './components/CreatorsMapaFome.js'
+
 import envVariables from './components/variaveisAmbiente';
 
 import qr from './images/qr.svg';
@@ -1251,6 +1254,8 @@ class App extends Component {
               
               <br></br>
               <br></br>
+              <CreatorsMapaFome/>
+              <br/>
 			  em agradecimento a formação humana, moral e ética que recebi dos meus professores de Filosofia e Sociologia do ensino médio
 			  <br/><span style={{"font-size":"1.4em","color":"blue"}}>•</span>por terem passado o premiado documentário com base em fatos reais curta de 13 minutos do brasileiro Jorge Furtado <a target='_blank' rel="noreferrer" href="https://www.youtube.com/watch?v=JcP9v5mZT9w">Ilha das Flores</a>
 			  <br/><span style={{"font-size":"1.4em","color":"blue"}}>•</span>após 10 anos de ter assistido, aprendido e internalizado e entendido nosso papel como sociedade e comunidade, tive a oportunidade de agir usando conhecimento e tecnologias acumulado ao longo do tempo 
@@ -1260,7 +1265,7 @@ class App extends Component {
 			  <br/><span style={{"font-size":"1.4em","color":"green"}}>•</span>e contribuir com os comerciantes de alimentos a reduzirem o desperdício de alimento
 			  <br/><span style={{"font-size":"1.4em","color":"red"}}>•</span>e motivar e ofertar ferramentas necessárias para cada ser humano fazer sua parte e poder colaborar de forma recorrente, saber e encontrar a quem ofertar alimento
 			  <br/><span style={{"font-size":"1.4em","color":"orange"}}>•</span>ou se não com o alimento, com o compartilhamento de informação ao informar da existência do MAPA FOME que é gratuito, a quem precisa e a quem pode ajudar, pois irão saber a quem buscar
-			  <br/><span style={{"font-size":"1.4em","color":"red"}}>•</span>sem comida, qualquer ser humano morre prematuramente. e qualquer pessoa ao deixar de prestar assistência, quando possível fazê-lo sem risco pessoal, à pessoa (...) ao desamparo ou em grave e iminente perigo => praticar crime de Omissão de socorro Art. 135 Código Penal Brasileiro
+			  <br/><span style={{"font-size":"1.4em","color":"red"}}>•</span>sem comida, qualquer ser humano morre prematuramente. e qualquer pessoa ao deixar de prestar assistência, quando possível fazê-lo sem risco pessoal, à pessoa (...) ao desamparo ou em grave e iminente perigo = praticar crime de Omissão de socorro Art. 135 Código Penal Brasileiro
 			  
 			  <table style={{"width":"94%", "text-align":"center"}}>
 				  <tr style={{"background-color":"#c8dff5"}}>
@@ -1324,7 +1329,7 @@ class App extends Component {
 					<td>extremamente alto</td>
 				  </tr>
 				</table>
-        <br/>Conclusão: Faço um apelo a você tomar medidas e ações solidárias e qualquer forma de iniciativa recorrentes, em intervalos de 1 dia a 14 dias (mínimo diário ou menos, máximo 2 semanas ou menos), alivie a dor e sofrimento de outro ser humano
+        <br/>Conclusão: Faço um apelo a você tomar medidas e ações solidárias e qualquer forma de iniciativa recorrentes, em intervalos de 1 dia a 14 dias (intervalos recorrentes de mínimo diário ou menos, máximo 2 semanas ou menos), alivie a dor e sofrimento de outro ser humano
 				<br/>
               
                 {/* <img src={qr} alt=""/> */}
