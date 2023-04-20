d40dc0d890cbe8b9534baa8e32be14fb5d439204
rslgp <rslgp@cin.ufpe.br>
2023-04-20 06:09:57 -0300

    Create CreatorsMapaFome.js

---
+++ src/components/CreatorsMapaFome.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CreatorsMapaFome = () => {
+  return (
+    <div>
+      <h1>Criadores de Conteúdo do Mapa Fome</h1>
+      <iframe
+        src="http://creators.mapafome.com.br/"
+        title="Creators Mapa Fome"
+        style={{ width: '100%', height: '500px' }}
+        frameBorder="0"
+        scrolling="no"
+      />
+    </div>
+  );
+};
+
+export default CreatorsMapaFome;
